81dc0807326278e6da9fd5cbfdbf9a8b9a6851c3
grup107 <yazilim107@gmail.com>
2020-01-19 14:23:46 +0300

    dfdsf

--- src/com/hokumus/course/project/main/Runner.java
+++ src/com/hokumus/course/project/main/Runner.java
@@ -6,15 +6,6 @@ import com.hokumus.course.project.utils.dao.DbServicessBase;
 public class Runner {
 	
 	public static void main(String[] args) {
-		//Utku
-		//heo7
-		//halilerkanözcan
-		//Omer Faruk
-		//kübra
-		// Kalkýn Ey Ehl_i Vatan Dediler, Kalktýk; Herkes Oturdu Biz Ayakta Kaldýk./B.KUS
-		//samet was here
-		//Bi commit
-		//yasin
 		DbServicessBase<CourseUsers> dao = new DbServicessBase<CourseUsers>();
 		dao.getAllRows(new CourseUsers());
 	}
