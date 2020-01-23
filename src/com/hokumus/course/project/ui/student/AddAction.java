84f6fcec5c2d2b5b9b536a7a80b464f7113abf50
Kübra <Kbra@DESKTOP-J1G5C05>
2020-01-23 17:38:54 +0300

    Kübra

--- src/com/hokumus/course/project/ui/student/AddAction.java
+++ src/com/hokumus/course/project/ui/student/AddAction.java
@@ -6,6 +6,8 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import com.hokumus.course.project.models.student.Student;
 import com.hokumus.course.project.utils.dao.StudentDao;
+
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -21,14 +23,13 @@ import javax.swing.JTextField;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-public class AddAction extends JFrame {
+public class AddAction extends JFrame { //??????
 
 	private JTable table;
 	private JTextField txtKullaniciAdi;
 	private JTextField txtEmail;
 	private JTextField txtAdi;
 	private JTextField txtSoyadi;
-	private int id;
 
 	public AddAction() {
 		intialize();
@@ -38,7 +39,8 @@ public class AddAction extends JFrame {
 		setBounds(100, 100, 609, 492);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		getContentPane().setLayout(null);
-
+		setBackground(Color.BLACK);
+		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 162, 572, 179);
 		getContentPane().add(scrollPane);
@@ -48,62 +50,61 @@ public class AddAction extends JFrame {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				txtKullaniciAdi.setText(table.getValueAt(table.getSelectedRow(), 1).toString());
-				txtAdi.setText(table.getValueAt(table.getSelectedRow(), 3).toString());
 				txtEmail.setText(table.getValueAt(table.getSelectedRow(), 2).toString());
+				txtAdi.setText(table.getValueAt(table.getSelectedRow(), 3).toString());
 				txtSoyadi.setText(table.getValueAt(table.getSelectedRow(), 4).toString());
 			}
 		});
 		scrollPane.setViewportView(table);
 		tabloDoldur();
-		
+
 		JButton btnKullaniciGetir = new JButton("Kullan\u0131c\u0131lar\u0131 Getir");
-		btnKullaniciGetir.setFont(new Font("arial", Font.TRUETYPE_FONT, 12));
 		btnKullaniciGetir.setBounds(10, 352, 572, 23);
+		btnKullaniciGetir.setFont(new Font("Arial", Font.BOLD, 14));
 		getContentPane().add(btnKullaniciGetir);
 
 		txtKullaniciAdi = new JTextField();
 		txtKullaniciAdi.setBounds(124, 21, 141, 20);
 		getContentPane().add(txtKullaniciAdi);
 		txtKullaniciAdi.setColumns(10);
-		
 
 		JLabel lblKullaniciAdi = new JLabel("Kullan\u0131c\u0131 Ad\u0131   *");
+		lblKullaniciAdi.setFont(new Font("Arial", Font.BOLD, 12));
 		lblKullaniciAdi.setBounds(10, 24, 93, 14);
 		getContentPane().add(lblKullaniciAdi);
-	
 
 		JLabel lblEmail = new JLabel("Email   *");
+		lblEmail.setFont(new Font("Arial", Font.BOLD, 12));
 		lblEmail.setBounds(287, 24, 78, 14);
 		getContentPane().add(lblEmail);
-		
 
 		txtEmail = new JTextField();
-		txtEmail.setColumns(10);
 		txtEmail.setBounds(383, 21, 154, 20);
+		txtEmail.setColumns(10);
 		getContentPane().add(txtEmail);
 
 		JLabel lblAdi = new JLabel("Ad\u0131");
+		lblAdi.setFont(new Font("Arial", Font.BOLD, 12));
 		lblAdi.setBounds(10, 70, 78, 14);
 		getContentPane().add(lblAdi);
-	
 
 		txtAdi = new JTextField();
-		txtAdi.setColumns(10);
 		txtAdi.setBounds(124, 67, 141, 20);
+		txtAdi.setColumns(10);
 		getContentPane().add(txtAdi);
 
 		JLabel lblSoyadi = new JLabel("Soyad\u0131");
+		lblSoyadi.setFont(new Font("Arial", Font.BOLD, 12));
 		lblSoyadi.setBounds(287, 70, 78, 14);
 		getContentPane().add(lblSoyadi);
-		
 
 		txtSoyadi = new JTextField();
-		txtSoyadi.setColumns(10);
 		txtSoyadi.setBounds(383, 70, 154, 20);
+		txtSoyadi.setColumns(10);
 		getContentPane().add(txtSoyadi);
-		
 
 		JButton btnGuncelle = new JButton("G\u00FCncelle");
+		btnGuncelle.setBounds(383, 112, 154, 23);
 		btnGuncelle.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				StudentDao studentdao = new StudentDao();
@@ -111,16 +112,14 @@ public class AddAction extends JFrame {
 				student.setAd(txtAdi.getText());
 				student.setSoyad(txtSoyadi.getText());
 				student.setMail(txtEmail.getText());
-
 				if (studentdao.update(student)) {
 					JOptionPane.showMessageDialog(AddAction.this, "Kayýt Baþarýlý..!");
 				} else
 					JOptionPane.showMessageDialog(AddAction.this, "Kayýt Baþarýsýz..!");
 			}
 		});
-		btnGuncelle.setBounds(383, 112, 154, 23);
 		getContentPane().add(btnGuncelle);
-		btnGuncelle.setFont(new Font("arial", Font.TRUETYPE_FONT, 12));
+		btnGuncelle.setFont(new Font("Arial", Font.BOLD, 14));
 		setTitle("Dersane Uygulamasý ");
 
 		JMenuBar menuBarKullaniciIslem = new JMenuBar();
@@ -146,13 +145,11 @@ public class AddAction extends JFrame {
 		});
 	}
 
-
-
 	private void tabloDoldur() {
 		StudentDao studentdao = new StudentDao();
 		List<Student> liste = studentdao.getAllRows(new Student());
 		String[][] data = new String[liste.size()][7];
-		String[] columns = { "Id", "Adý", "Soyadý", "Tel No", "Kayýt Tarihi", "E-Mail", "Adres"  };
+		String[] columns = { "Id", "Adý", "Soyadý", "Tel No", "Kayýt Tarihi", "E-Mail", "Adres" };
 		for (int i = 0; i < liste.size(); i++) {
 			data[i][0] = String.valueOf(liste.get(i).getId());
 			data[i][1] = liste.get(i).getAd();
@@ -161,7 +158,7 @@ public class AddAction extends JFrame {
 			data[i][4] = String.valueOf(liste.get(i).getKayitTarihi());
 			data[i][5] = liste.get(i).getMail();
 			data[i][6] = liste.get(i).getAdres();
-			
+
 		}
 		DefaultTableModel model = new DefaultTableModel(data, columns);
 		table.setModel(model);
--- src/com/hokumus/course/project/ui/student/AddUsers.java
+++ src/com/hokumus/course/project/ui/student/AddUsers.java
@@ -2,109 +2,126 @@ package com.hokumus.course.project.ui.student;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
-
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JPasswordField;
 
+import com.hokumus.course.project.models.student.Student;
+
+import com.hokumus.course.project.utils.dao.StudentDao;
+
 import com.toedter.calendar.JDateChooser;
 
-public class AddUsers extends JFrame{
-	
+public class AddUsers extends JFrame {  //???????
+
 	private JTextField txtAdi;
 	private JTextField txtTelNo;
 	private JTextField txtAdress;
 	private JTextField txtMail;
 	private JPasswordField txtSoyadi;
 	private JDateChooser dateKayitTarihi;
-	
+
 	public AddUsers() {
 		initialize();
 	}
 
 	private void initialize() {
-		
+		setTitle("KAYIT");
 		setBounds(300, 300, 550, 300);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		getContentPane().setLayout(null);
-		
+
 		JLabel lblAdi = new JLabel("Ad\u0131 ");
+		lblAdi.setFont(new Font("Arial", Font.BOLD, 12));
 		lblAdi.setBounds(23, 24, 67, 14);
 		getContentPane().add(lblAdi);
-		
+
 		txtAdi = new JTextField();
 		txtAdi.setBounds(100, 21, 158, 20);
 		getContentPane().add(txtAdi);
 		txtAdi.setColumns(10);
-		
+
 		JLabel lblSoyadi = new JLabel("Soyad");
+		lblSoyadi.setFont(new Font("Arial", Font.BOLD, 12));
 		lblSoyadi.setBounds(282, 27, 67, 14);
 		getContentPane().add(lblSoyadi);
-		
+
 		JLabel lblTel = new JLabel("Telefon No");
+		lblTel.setFont(new Font("Arial", Font.BOLD, 12));
 		lblTel.setBounds(23, 75, 67, 14);
 		getContentPane().add(lblTel);
-		
+
 		txtTelNo = new JTextField();
 		txtTelNo.setColumns(10);
 		txtTelNo.setBounds(100, 72, 158, 20);
 		getContentPane().add(txtTelNo);
-		
+
 		JLabel lblKytTrihi = new JLabel("Kay\u0131t Tarihi");
+		lblKytTrihi.setFont(new Font("Arial", Font.BOLD, 12));
 		lblKytTrihi.setBounds(282, 78, 67, 14);
 		getContentPane().add(lblKytTrihi);
-		
+
 		JLabel lblAdress = new JLabel("Adres");
+		lblAdress.setFont(new Font("Arial", Font.BOLD, 12));
 		lblAdress.setBounds(23, 128, 67, 14);
 		getContentPane().add(lblAdress);
-		
+
 		txtAdress = new JTextField();
 		txtAdress.setColumns(10);
 		txtAdress.setBounds(100, 125, 158, 82);
 		getContentPane().add(txtAdress);
-		
+
 		JLabel lblMail = new JLabel("Email");
+		lblMail.setFont(new Font("Arial", Font.BOLD, 12));
 		lblMail.setBounds(282, 128, 67, 14);
 		getContentPane().add(lblMail);
-		
-		
+
 		JButton btnIptal = new JButton("\u0130ptal");
-		btnIptal.setBounds(297, 184, 91, 23);
-		btnIptal.setFont(new Font("arial", Font.TRUETYPE_FONT, 12));
+		btnIptal.setBounds(282, 215, 91, 23);
+		btnIptal.setFont(new Font("Arial", Font.BOLD, 12));
 		getContentPane().add(btnIptal);
-		btnIptal.addActionListener(new ActionListener() {			
+		btnIptal.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				AddUsers.this.dispose();				
+				AddUsers.this.dispose();
 			}
 		});
-		
+
 		JButton btnKaydet = new JButton("Kaydet");
-		btnKaydet.setBounds(398, 184, 91, 23);
-		btnKaydet.setFont(new Font("arial", Font.TRUETYPE_FONT, 12));
+		btnKaydet.setBounds(394, 215, 91, 23);
+		btnKaydet.setFont(new Font("Arial", Font.BOLD, 12));
 		getContentPane().add(btnKaydet);
-		btnKaydet.addActionListener(new ActionListener() {			
+		btnKaydet.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-							
+				StudentDao studentDao = new StudentDao();
+				Student student = new Student();
+				student.setAd(txtAdi.getText());
+				student.setSoyad(txtSoyadi.getText()); // ?????????
+				student.setAdres(txtAdress.getText()); 
+				student.setMail(txtMail.getText());
+				if (studentDao.save(student)) {
+					JOptionPane.showMessageDialog(AddUsers.this, "Kayýt Baþarýlý..!");
+				} else
+					JOptionPane.showMessageDialog(AddUsers.this, "Baþarýsýz..!");
 			}
+
 		});
-		
+
 		txtMail = new JTextField();
 		txtMail.setColumns(10);
 		txtMail.setBounds(359, 125, 130, 20);
 		getContentPane().add(txtMail);
-		
-		
+
 		txtSoyadi = new JPasswordField();
 		txtSoyadi.setBounds(358, 21, 131, 20);
 		getContentPane().add(txtSoyadi);
-		
+
 		dateKayitTarihi = new JDateChooser();
 		dateKayitTarihi.setBounds(359, 75, 130, 20);
 		getContentPane().add(dateKayitTarihi);
-		setTitle("KAYIT");
-		
+
 	}
 }
