fe68615aa78e943be5f6402e086af80593ecea70
heo <heo@192.168.1.22>
2020-01-28 00:51:57 +0300

    fatura ekranı dosya ekleme

--- src/com/hokumus/course/project/ui/accounting/Bills.java
+++ src/com/hokumus/course/project/ui/accounting/Bills.java
@@ -2,8 +2,19 @@ package com.hokumus.course.project.ui.accounting;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class Bills extends JFrame {
+	private JButton btnDosyaSec;
+	private JLabel lblbos;
 	
 	public Bills() {
 		
@@ -17,18 +28,49 @@ public class Bills extends JFrame {
 		setBounds(300, 300, 500, 500);
 		getContentPane().setLayout(null);
 		
-		JLabel lblFaturaKaydet = new JLabel("Fatura Kaydet");
-		lblFaturaKaydet.setBounds(71, 36, 83, 14);
+		JLabel lblFaturaKaydet = new JLabel("Fatura Se\u00E7iniz");
+		lblFaturaKaydet.setBounds(43, 36, 83, 14);
 		getContentPane().add(lblFaturaKaydet);
 		
 		JLabel lblFaturaGrntle = new JLabel("Fatura G\u00F6r\u00FCnt\u00FCle");
 		lblFaturaGrntle.setBounds(287, 36, 108, 14);
 		getContentPane().add(lblFaturaGrntle);
-		
-		JLabel lblFilechooser = new JLabel("filechooser yap");
-		lblFilechooser.setBounds(189, 120, 101, 14);
-		getContentPane().add(lblFilechooser);
+		getContentPane().add(getBtnDosyaSec());
+		getContentPane().add(getLblbos());
 		
 	}
-
+	private JButton getBtnDosyaSec() {
+		if (btnDosyaSec == null) {
+			btnDosyaSec = new JButton("Dosya Se\u00E7");
+			btnDosyaSec.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					
+					JFileChooser fc = new JFileChooser();
+					fc.setDialogTitle("Fatura Seçme Ekraný");
+					fc.setApproveButtonText("Seç");
+					fc.addChoosableFileFilter(new FileNameExtensionFilter("Pdf Belgesi", "pdf"));
+					
+					int result = fc.showOpenDialog(null);
+					if (result == JFileChooser.APPROVE_OPTION) {
+						
+						lblbos.setText(fc.getSelectedFile().getName());
+					}
+					else if (result == JFileChooser.CANCEL_OPTION) {
+						lblbos.setText("Fatura Seçilmedi.");
+					}
+					
+				}
+			});
+			btnDosyaSec.setBounds(43, 88, 108, 23);
+		}
+		return btnDosyaSec;
+	}
+	private JLabel getLblbos() {
+		if (lblbos == null) {
+			lblbos = new JLabel("");
+			lblbos.setForeground(Color.RED);
+			lblbos.setBounds(43, 61, 152, 14);
+		}
+		return lblbos;
+	}
 }
