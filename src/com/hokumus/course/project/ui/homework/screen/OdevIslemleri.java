56aca188dd1539840e2891e101993e4951b6b8af
Sony <Sony@Sony-Bilgisayar>
2020-02-02 14:25:29 +0300

    samet

---
+++ src/com/hokumus/course/project/ui/homework/screen/OdevIslemleri.java
@@ -0,0 +1,9 @@
+package com.hokumus.course.project.ui.homework.screen;
+
+import javax.swing.JFrame;
+
+public class OdevIslemleri extends JFrame {
+	public OdevIslemleri() {
+	}
+
+}
---
+++ src/com/hokumus/course/project/ui/homework/screen/RunnerHomework.java
@@ -0,0 +1,8 @@
+package com.hokumus.course.project.ui.homework.screen;
+
+public class RunnerHomework {
+	public static void main(String[] args) {
+		new HomeworkMainScreen().setVisible(true);
+	}
+
+}
