9601c2dc1d9eee5cc07e1b5977bacda25006a42a
Yasin <pc@DESKTOP-KKDOO3H>
2020-02-06 17:45:08 +0300

    ufak bir düzeltme

---
+++ src/com/hokumus/course/project/utils/dao/IncomingsDAO.java
@@ -0,0 +1,7 @@
+package com.hokumus.course.project.utils.dao;
+
+import com.hokumus.course.project.models.accounting.Incomings;
+
+public class IncomingsDAO extends DbServicessBase<Incomings> {
+
+}
